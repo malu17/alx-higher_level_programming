733a91d0765161a6873b1bd66256cb46ef7cf997
Lesm <88344019+malu17@users.noreply.github.com>
2022-04-12 19:59:59 +0300

    Update 100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
 const epId = process.argv[2];
-const apiUrl = `https://swapi.co/api/films/${epId}`;
 request.get(apiUrl, (error, response, body) => {
   if (error) {
     console.log(error);
