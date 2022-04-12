b929b5a57f13c573067dedf2b3e2c0de55c40f37
Lesm <88344019+malu17@users.noreply.github.com>
2022-04-12 20:48:36 +0300

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,17 +1,19 @@
 #!/usr/bin/node
-
-const r = require('request');
-
-r.get(process.argv[2], (err, res, body) => {
-  if (err) console.log(err);
-  else {
-    let count = 0;
-    const movies = JSON.parse(body).results;
-    movies.forEach(movie => {
-      movie.characters.forEach(character => {
-        if (character.includes('people/18/')) count++;
-      });
-    });
-    console.log(count);
+// JS Script
+require('request').get(process.argv[2], function (err, r, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    let counter = 0;
+    let data = JSON.parse(body).results;
+    for (let i = 0; i < data.length; i++) {
+      for (let n = 0; n < data[i].characters.length; n++) {
+        if (data[i].characters[n].includes('/18/')) {
+          counter++;
+          break;
+        }
+      }
+    }
+    console.log(counter);
   }
 });
