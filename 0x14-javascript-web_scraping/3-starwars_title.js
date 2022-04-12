649da190ca395eaaddb71bf4be30c93cfb5ad648
Lesm <88344019+malu17@users.noreply.github.com>
2022-04-12 20:41:30 +0300

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,14 @@
 #!/usr/bin/node
-
-const r = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-
-r.get(url, (err, res, body) => {
-  if (err) console.log(err);
-  else console.log(JSON.parse(body).title);
+const request = require('request');
+let url = 'http://swapi.co/api/films/';
+let episode = process.argv[2];
+request(url + episode, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    let jsonobj = JSON.parse(body);
+    console.log(jsonobj.title);
+  } else {
+    console.log('An error occured. Status code: ' + response.statusCode);
+  }
 });
