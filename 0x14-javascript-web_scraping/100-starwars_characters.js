bd5adc3350f070b83e6503abefd19074facb980c
Lesm <88344019+malu17@users.noreply.github.com>
2022-04-12 20:09:25 +0300

    Update 100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,21 +1,16 @@
 #!/usr/bin/node
-const request = require('request');
-const epId = process.argv[2];
-const apiUrl = `https://swapi.co/api/films/${epId}`;
-request.get(apiUrl, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const charList = JSON.parse(body).characters;
-    for (let i = 0; i < charList.length; i++) {
-      request.get(charList[i], (err, res, body) => {
-        if (err) {
-          console.log(err);
-        } else {
-          const names = JSON.parse(body).name;
-          console.log(names);
-        }
+
+const r = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+r.get(url, (err, res, body) => {
+  if (err) console.log(err);
+  else {
+    JSON.parse(body).characters.forEach(character => {
+      r.get(character, (err, res, body) => {
+        if (err) console.log(err);
+        else console.log(JSON.parse(body).name);
       });
-    }
+    });
   }
 });
