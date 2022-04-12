ea06352a502ef391f2e3b1cfd987288d4bda6f60
Lesm <88344019+malu17@users.noreply.github.com>
2022-04-12 20:06:32 +0300

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,21 +1,17 @@
 #!/usr/bin/node
-const request = require('request');
-const apiUrl = process.argv[2];
 
-request.get(apiUrl, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    let countWedge = 0;
-    const films = JSON.parse(body).results;
-    for (let i = 0; i < films.length; i++) {
-      const charList = films[i].characters;
-      for (let j = 0; j < charList.length; j++) {
-        if (charList[j] === 'https://swapi.co/api/people/18/' || charList[j] === 'http://swapi.co/api/people/18/') {
-          countWedge += 1;
-        }
-      }
-    }
-    console.log(countWedge);
+const r = require('request');
+
+r.get(process.argv[2], (err, res, body) => {
+  if (err) console.log(err);
+  else {
+    let count = 0;
+    const movies = JSON.parse(body).results;
+    movies.forEach(movie => {
+      movie.characters.forEach(character => {
+        if (character.includes('people/18/')) count++;
+      });
+    });
+    console.log(count);
   }
 });
